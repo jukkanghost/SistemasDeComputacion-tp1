d7047b93d91f0fc02d5a948ec38930c21ccee21c
Francisco <francisco.daniele@mi.unc.edu.ar>
2021-04-11 16:10:37 -0300

    hecho cambio de gui binaria a decimal

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ main:
 	nasm -f elf restaDecimal.asm
 	nasm -f elf sumaBinaria.asm
 	nasm -f elf restaBinaria.asm
-	gcc -m32  -c -o calculadora.o calculadora.c
-	gcc -m32  sumaDecimal.o restaDecimal.o restaBinaria.o sumaBinaria.o calculadora.o  -o calc
+	gcc -m32 -c -o calculadora.o calculadora.c
+	gcc -m32 sumaDecimal.o restaDecimal.o restaBinaria.o sumaBinaria.o calculadora.o -o calc
 clean: 
 		rm -f calc *.o
--- gui_calc.c
+++ gui_calc.c
@@ -63,16 +63,82 @@ void num3_action(){
     gtk_entry_set_text((GtkEntry *)input, inputNum);
 }
 
+void num4_action(){
+    char inputNum[30] = "";
+    char four = '4';
+    int inputInt;
+
+    sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &inputInt);
+    sprintf(inputNum, "%i", inputInt);
+    strncat(inputNum, &four, 1);
+    gtk_entry_set_text((GtkEntry *)input, inputNum);
+}
+
+void num5_action(){
+    char inputNum[30] = "";
+    char five = '5';
+    int inputInt;
+
+    sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &inputInt);
+    sprintf(inputNum, "%i", inputInt);
+    strncat(inputNum, &five, 1);
+    gtk_entry_set_text((GtkEntry *)input, inputNum);
+}
+
+void num6_action(){
+    char inputNum[30] = "";
+    char six = '6';
+    int inputInt;
+
+    sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &inputInt);
+    sprintf(inputNum, "%i", inputInt);
+    strncat(inputNum, &six, 1);
+    gtk_entry_set_text((GtkEntry *)input, inputNum);
+}
+
+void num7_action(){
+    char inputNum[30] = "";
+    char seven = '7';
+    int inputInt;
+
+    sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &inputInt);
+    sprintf(inputNum, "%i", inputInt);
+    strncat(inputNum, &seven, 1);
+    gtk_entry_set_text((GtkEntry *)input, inputNum);
+}
+
+void num8_action(){
+    char inputNum[30] = "";
+    char eigth = '8';
+    int inputInt;
+
+    sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &inputInt);
+    sprintf(inputNum, "%i", inputInt);
+    strncat(inputNum, &eigth, 1);
+    gtk_entry_set_text((GtkEntry *)input, inputNum);
+}
+
+void num9_action(){
+    char inputNum[30] = "";
+    char nine = '9';
+    int inputInt;
+
+    sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &inputInt);
+    sprintf(inputNum, "%i", inputInt);
+    strncat(inputNum, &nine, 1);
+    gtk_entry_set_text((GtkEntry *)input, inputNum);
+}
+
 void sum_action(){
     operacion = 0;
     sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &op1);
-    gtk_entry_set_text((GtkEntry *)input, "");
+    gtk_entry_set_text((GtkEntry *)input, "0");
 }
 
 void sub_action(){
     operacion = 1;
     sscanf(gtk_entry_get_text(GTK_ENTRY(input)), "%i", &op1);
-    gtk_entry_set_text((GtkEntry *)input, "");
+    gtk_entry_set_text((GtkEntry *)input, "0");
 }
 
 void eq_action(){
@@ -92,6 +158,27 @@ void eq_action(){
 }
 
 void change_action(){
+    if(change_count%2 == 0){
+        gtk_button_set_label ((GtkButton *)change_button, "Dec");
+        
+        gtk_layout_move(GTK_LAYOUT(layout1), symbols[0], 80, 100);
+        gtk_layout_move(GTK_LAYOUT(layout1), symbols[1], 80, 150);
+        for(int i=2; i<10; i++){
+            gtk_layout_move(GTK_LAYOUT(layout1), symbols[i], -100, -100);
+        }
+    }
+    else{
+        gtk_button_set_label ((GtkButton *)change_button, "Bin");
+
+        gtk_layout_move(GTK_LAYOUT(layout1), symbols[0], 30, 250);
+        for (int i = 0; i < 3; i++)
+        {
+            gtk_layout_move(GTK_LAYOUT(layout1), symbols[i+1], 30+i*50, 100);
+            gtk_layout_move(GTK_LAYOUT(layout1), symbols[i+4], 30+i*50, 150);
+            gtk_layout_move(GTK_LAYOUT(layout1), symbols[i+7], 30+i*50, 200);
+        }
+    }
+
     change_count++;
 }
 
@@ -99,7 +186,7 @@ void clear_action(){
     op1 = 0;
     op2 = 0;
     result = 0;
-    gtk_entry_set_text((GtkEntry *)input, "");
+    gtk_entry_set_text((GtkEntry *)input, "0");
 }
 
 int main(int argc, char **argv){
@@ -119,7 +206,7 @@ int main(int argc, char **argv){
     input = gtk_entry_new();
     gtk_entry_set_max_length(GTK_ENTRY(input), 25);
     gtk_entry_set_width_chars(GTK_ENTRY(input), 25);
-    gtk_entry_set_text((GtkEntry *)input, "");
+    gtk_entry_set_text((GtkEntry *)input, "0");
     
     //creo botones de 0 a 9
     symbols[0] = gtk_button_new_with_label("0");
@@ -159,6 +246,12 @@ int main(int argc, char **argv){
     g_signal_connect(symbols[1], "clicked", G_CALLBACK(num1_action), NULL);
     g_signal_connect(symbols[2], "clicked", G_CALLBACK(num2_action), NULL);
     g_signal_connect(symbols[3], "clicked", G_CALLBACK(num3_action), NULL);
+    g_signal_connect(symbols[4], "clicked", G_CALLBACK(num4_action), NULL);
+    g_signal_connect(symbols[5], "clicked", G_CALLBACK(num5_action), NULL);
+    g_signal_connect(symbols[6], "clicked", G_CALLBACK(num6_action), NULL);
+    g_signal_connect(symbols[7], "clicked", G_CALLBACK(num7_action), NULL);
+    g_signal_connect(symbols[8], "clicked", G_CALLBACK(num8_action), NULL);
+    g_signal_connect(symbols[9], "clicked", G_CALLBACK(num9_action), NULL);
 
     //dibujo +, -, = y cambio, le pongo acciones
     gtk_layout_put(GTK_LAYOUT(layout1), p_button, 80, 250);
