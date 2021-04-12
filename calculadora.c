0ec13ba973318a455606c8bbd35a46421f7b8462
jukkanghost <jukkanghost@gmail.com>
2021-04-12 20:54:59 -0300

    validacion testeo parametros

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,8 @@
 #include "cdecl.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int PRE_CDECL asm_main(void) POST_CDECL;
 int PRE_CDECL sumaDecimal(int, int) POST_CDECL;
@@ -8,8 +10,10 @@ int PRE_CDECL restaDecimal(int, int) POST_CDECL;
 int PRE_CDECL sumaBinaria(char, char, char, char, char, char, char, char) POST_CDECL;
 int PRE_CDECL restaBinaria(char, char, char, char, char, char, char, char) POST_CDECL;
 
-void decabin (int n) {
-    if (n) {
+void decabin(int n)
+{
+    if (n)
+    {
         decabin(n / 2);
         printf("%d", n % 2);
     }
@@ -20,12 +24,49 @@ int main(int argc, char *argv[])
 
     int a, b, resultado;
     resultado = 0;
-    int esDecimal = atoi(argv[1]);
+    int operacion = atoi(argv[1]);
     char binario1[4];
     char binario2[4];
-    
 
-    if(esDecimal){
+
+
+    if (isdigit(argv[1][0]) == 0)
+    {
+        printf("Elige 0 o 1 para las operaciones\n");
+        return EXIT_FAILURE;
+    }
+
+    if (operacion < 0 || operacion > 1)
+    {
+        printf("Elige 0 o 1 para las operaciones\n");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < strlen(argv[2]); i++)
+    {
+        if (isdigit(argv[2][i]) == 0)
+        {
+            printf("Pone numeros\n");
+            return EXIT_FAILURE;
+        }
+    }
+    for (int i = 0; i < strlen(argv[4]); i++)
+    {
+        if (isdigit(argv[4][i]) == 0)
+        {
+            printf("Pone numeros\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (operacion)
+    {
+        if (strlen(argv[2]) > 8 || strlen(argv[4]) > 8)
+        {
+            printf("Numero muy grande\n");
+            return EXIT_FAILURE;
+        }
+
         a = atoi(argv[2]);
         b = atoi(argv[4]);
 
@@ -39,8 +80,32 @@ int main(int argc, char *argv[])
         }
         printf("%i\n", resultado);
     }
-    else{
-        for(int j=0; j<4; j++){
+    else
+    {
+        if (strlen(argv[2]) != 4 || strlen(argv[4]) != 4)
+        {
+            printf("Binario de 4 digitos\n");
+            return EXIT_FAILURE;
+        }
+        for (int i = 0; i < strlen(argv[2]); i++)
+        {
+            if (argv[2][i] < 48 || argv[2][i] > 49)
+            {
+                printf("%i %i %c", strlen(argv[2]), i, (argv[2][i]));
+                printf("Solo ceros y unos\n");
+                return EXIT_FAILURE;
+            }
+        }
+        for (int i = 0; i < strlen(argv[4]); i++)
+        {
+            if (argv[4][i] < 48 || argv[4][i] > 49)
+            {
+                printf("Solo ceros y unos\n");
+                return EXIT_FAILURE;
+            }
+        }
+        for (int j = 0; j < 4; j++)
+        {
             binario1[j] = (char)argv[2][j];
             binario2[j] = (char)argv[4][j];
         }
@@ -52,7 +117,7 @@ int main(int argc, char *argv[])
 
         if ((char)argv[3][0] == '-')
         {
-            resultado = restaBinaria(binario1[0], binario1[1], binario1[2], binario1[3], binario2[0], binario2[1], binario2[2], binario2[3]);            
+            resultado = restaBinaria(binario1[0], binario1[1], binario1[2], binario1[3], binario2[0], binario2[1], binario2[2], binario2[3]);
         }
         decabin(resultado);
         printf("\n");
--- test.sh
+++ test.sh
@@ -23,4 +23,23 @@ assert_equals "$response" 10
 response=$(./calc 0 1100 - 0100)
 assert_equals "$response" 1000
 
+response=$(/calc 0 1100 + 100f)
+assert_equals "$response" Pone numeros
+
+response=$(./calc 0 1100 - 10000)
+assert_equals "$response" Binario de 4 digitos
+
+response=$(./calc 0 1300 + 1000)
+assert_equals "$response" Solo ceros y unos
+
+response=$(./calc 1 454555 - 101823dsf)
+assert_equals "$response" Pone numeros
+
+response=$(./calc 1 454555 + 10182391893198)
+assert_equals "$response" Numero muy grande
+
+response=$(./calc akjfh 1100 + 1100)
+assert_equals "$response" Elige 0 o 1 para las operaciones
+
+
 
