86038580f9162ae88f6fd2f546fa0cb103770f06
Francisco <francisco.daniele@mi.unc.edu.ar>
2021-05-21 16:07:51 -0300

    organizado + tp modo protegido

--- prot-mode/prot-mode.asm
+++ prot-mode/prot-mode.asm
@@ -10,7 +10,7 @@
     mov bh, 0x7
     mov cx, 0x0
     mov dx, 0x184f
-    int 0x10  
+    int 0x10                ;se limpia la pantalla
 
     push ax
     push bx
@@ -20,7 +20,7 @@
     mov bh, 0x00
     mov dh, 0x0
     mov dl, 0x0
-    int 0x10
+    int 0x10                ;se vuelve el cursor al principio    
     pop dx
     pop cx
     pop bx
@@ -31,15 +31,16 @@
     pop bx
     pop ax
 
-    cli
-    lgdt[gdt_descriptor]
-    mov eax, cr0
+    cli                     ;desactivo interrupcions
+    lgdt[gdt_descriptor]    ;cargo la gdt
+    mov eax, cr0            
     or al,1
-    mov cr0, eax
-    jmp CODE_SEG:b32
+    mov cr0, eax            ;activo bit 0 del cr0
+    jmp short $+2           ;vacio pipeline
+    jmp CODE_SEG:b32        ;salto al codigo de 32 bits
 
 [BITS 32]
-print32:
+print32:                    ;funcion para imprimir un mensaje en pantalla
     pusha
     mov edx, 0xb8000
 .loop:
@@ -55,27 +56,27 @@ print32:
     popa
     ret
 
-b32:
+b32:                        ;codigo que se ejecuta en modo protegido
     mov ax, DATA_SEG
     mov ds, ax
     mov es, ax
     mov fs, ax
-    mov gs, ax
-    mov ss, ax
+    mov gs, ax              ;le aviso a los selectores que manejan datos
+    mov ss, ax              ;el offset del data seg en la gdt
 
-    mov ebp, 0x2000
+    mov ebp, 0x2000         ;hago espacio para la pila
     mov esp, ebp
 
-    mov ebx, msg1
+    mov ebx, msg1           ;imprimo mensaje 1
     call print32
     jmp CODE_SEG:mp
 
 mp: 
-    in al,60h
+    in al,60h               ;espero que se pulse "esc" para poder continuar
     dec al
     jnz mp
 
-    mov ax, DATA_SEG
+    mov ax, DATA_SEG        
     mov ds, ax
     mov es, ax
     mov fs, ax
@@ -84,17 +85,16 @@ mp:
     mov ebp, 0x2000
     mov esp, ebp
     mov ebx, msg2
-    call print32
+    call print32            ;imprimo mensaje 2
 
-    mov eax,cr0
+    mov eax,cr0     
     and al,0feh
-
-    mov cr0,eax
+    mov cr0,eax             ;salgo de modo protegido
     jmp short $+2
 
-    sti
+    sti                     ;activo interrupciones
     mov ah, 4ch
-    int 21h
+    int 21h                 ;similar a return 0
 
 gdt_start:
 
@@ -103,18 +103,18 @@ gdt_null:
     dd 0x0
 
 gdt_code:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10011010b
-    db 11001111b
-    db 0x0
+    dw 0xffff               ;limit 00:15
+    dw 0x0                  ;base 00:15
+    db 0x0                  ;base 16:23
+    db 10011010b            ;P;DPL;S;TYPE;ACCEDIDO
+    db 11001111b            ;G;D/B;0;AVL;LIM 16:19
+    db 0x0                  ;base 24:31
 
 gdt_data:
     dw 0xffff
     dw 0x0
     db 0x0
-    db 10010010b    ;solo lectura, se rompe porque estoy poniendo un seg no escribible en ss
+    db 10010010b            ;solo lectura, se rompe porque estoy poniendo un seg no escribible en ss
     db 11001111b
     db 0x0
 
@@ -132,12 +132,3 @@ msg2 db 'salgo modo protegido', 0
 
 [SECTION signature start=0x7dfe]
 dw 0AA55h
-
-;un programa con dos descriptores de memoria diferentes p/ c/ seg (uno p/ datos y otro p/ codigo) 
-;en espacios de mem diferenciados sería como este
-
-;en modo protegido, los regs de seg se cargan con el indice que apunta a la entrada de dicho segmento en
-;la tabla de descriptores, que se obtiene cargando TI: local TI=1 global TI=1
-;los ultimos 2 bits son para el RPL
-;es así porque en modo protegido ya no apuntan a la base del segmento/16 sino que estan descriptos
-;por un descriptor ya que las propiedades que tienen son muchas y no se pueden contener mas en 16 bits
